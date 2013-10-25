c60ef9b888d4bb649a682ff866afc84a28b42e0c
William Archimede <william.archimede@hoodbrains.com>
2013-10-25 13:36:32 +0200

    Creation of a phone remote request.

--- Cozy Files/Cozy Files/CCViewController.m
+++ Cozy Files/Cozy Files/CCViewController.m
@@ -6,10 +6,13 @@
 //  Copyright (c) 2013 CozyCloud. All rights reserved.
 //
 
+#import "CCAppDelegate.h"
 #import "CCViewController.h"
 
-@interface CCViewController ()
+@interface CCViewController () <NSURLConnectionDataDelegate>
+@property (strong, nonatomic) NSMutableData *responseData;
 
+- (void)sendGetCredentialsRequestWithCozyURLString:(NSString *)cozyURL cozyPassword:(NSString *)cozyPassword remoteName:(NSString *)remoteName;
 @end
 
 @implementation CCViewController
@@ -48,5 +51,58 @@
 - (IBAction)okPressed:(id)sender
 {
     NSLog(@"%@\n%@\n%@", self.cozyUrlTextField.text, self.cozyMDPTextField.text, self.remoteNameTextField.text);
+    
+    [self sendGetCredentialsRequestWithCozyURLString:self.cozyUrlTextField.text cozyPassword:self.cozyMDPTextField.text remoteName:self.remoteNameTextField.text];
 }
+
+- (void)sendGetCredentialsRequestWithCozyURLString:(NSString *)cozyURL cozyPassword:(NSString *)cozyPassword remoteName:(NSString *)remoteName
+{
+    NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:[NSURL URLWithString:[NSString stringWithFormat:@"%@/apps/files/remotes", cozyURL]]];
+    
+    NSDictionary *requestData = [NSDictionary dictionaryWithObjectsAndKeys:remoteName, @"login", nil];
+    
+    NSError *error;
+    NSData *postData = [NSJSONSerialization dataWithJSONObject:requestData options:0 error:&error];
+    
+    if (error) {
+        CCAppDelegate *appDelegate = (CCAppDelegate *)[[UIApplication sharedApplication] delegate];
+        [appDelegate showAlert:@"Une erreur s'est produite" error:error fatal:NO];
+    } else {
+        NSString *base64Auth = [[[NSString stringWithFormat:@"owner:%@", cozyPassword] dataUsingEncoding:NSUTF8StringEncoding] base64EncodedStringWithOptions:0];
+        NSString *authValue = [NSString stringWithFormat:@"Basic %@", base64Auth];
+        
+        [request setValue:@"application/json" forHTTPHeaderField:@"Content-Type"];
+        [request setValue:authValue forHTTPHeaderField:@"Authorization"];
+        [request setValue:cozyPassword forHTTPHeaderField:@"X-Auth-Token"];
+        [request setHTTPMethod:@"POST"];
+        [request setHTTPBody:postData];
+        
+        NSURLConnection *connection = [[NSURLConnection alloc] initWithRequest:request delegate:self];
+        [connection start];
+    }
+}
+
+#pragma mark - Connection
+
+- (void)connection:(NSURLConnection *)connection didFailWithError:(NSError *)error
+{
+    CCAppDelegate *appDelegate = (CCAppDelegate *)[[UIApplication sharedApplication] delegate];
+    [appDelegate showAlert:@"Une erreur s'est produite" error:error fatal:NO];
+}
+
+- (void)connection:(NSURLConnection *)connection didReceiveResponse:(NSURLResponse *)response
+{
+    self.responseData = [NSMutableData data];
+}
+
+- (void)connection:(NSURLConnection *)connection didReceiveData:(NSData *)data
+{
+    [self.responseData appendData:data];
+}
+
+- (void)connectionDidFinishLoading:(NSURLConnection *)connection
+{
+    NSLog(@"RESPONSE - %@", [[NSString alloc] initWithData:self.responseData encoding:NSUTF8StringEncoding]);
+}
+
 @end
